7eb27b83c73fe42f6da1c9a8c1d739fdc6b5101d
bhenrywc <68617445+bhenrywc@users.noreply.github.com>
2026-05-11 17:49:53 -0400

    Update app.py

--- app/app.py
+++ app/app.py
@@ -489,7 +489,7 @@ def main():
         "📺 Streaming",
         "📱 Social Buzz",
         "▶️ Reviews & Trailers",
-        "🤖 Ask&Recieve",
+        "🤖 Ask FlikPik",
         "👤 Insights",
     ])
 
